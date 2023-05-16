8a800b74aa5d7b25a9a69f3e0cbce9d6f4e0712b
Jacob Christensen <jchristensen888@gmail.com>
2023-05-16 12:57:42 -0600

    chapter1: 1-13

---
+++ the_c_programming_lang/chapter1/wordLength.c
@@ -0,0 +1,48 @@
+// The C Programming Language
+// Excercise 1-13 
+// Author: Jacob Christensen
+
+#include <stdio.h>
+
+int main(void) {
+
+    int wordLengths[10];
+    int length = 0;
+    int character;
+    int i;
+    int j;
+
+    while((character = getchar()) != EOF) {
+        if (character == '\n' || character == ' ' || character ==  '\t') {
+            if (length == 0) {
+                length = 0;
+                continue;
+            }
+            else if (length > 10) {
+                wordLengths[9]++;
+            }
+            else {
+                wordLengths[length - 1]++;
+            }
+            length = 0; 
+        }
+        else {
+            length++;
+        }
+    }
+
+    printf("'=' = 10 occurances\n");
+    for (i = 0; i < 9; i++) {
+        printf("%d  : ", i + 1);
+        for (j = wordLengths[i] / 10; j > 0; j--) {
+            printf("=");
+        }
+        printf("\n");
+    }
+    printf("10+: ");
+    for (j = wordLengths[9] / 10; j > 0; j--) {
+        printf("=");
+    }
+    printf("\n");
+    return 0;
+}
