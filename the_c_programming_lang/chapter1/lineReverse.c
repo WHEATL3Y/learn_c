d5dc085adda8c5346c17213e7b7c94bbebe611ce
Jacob Christensen <jchristensen888@gmail.com>
2023-05-20 12:38:08 -0600

    chapter1: 1-18

---
+++ the_c_programming_lang/chapter1/lineReverse.c
@@ -0,0 +1,16 @@
+// The C Programming Language
+// Excercise 1-6, 1-7, 1-9 
+// Author: Jacob Christensen
+
+#include <stdio.h>
+
+int reverse(char string[], int length) {
+
+
+    
+}
+
+int main(void) {
+
+    return 0;
+}
