e8ce68e8951e18c7bcb751eeb7d70e19458ee6c0
Jacob Christensen <jchristensen888@gmail.com>
2023-06-07 21:34:34 -0600

    chapter2: getbits

---
+++ the_c_programming_lang/chapter2/getBits.c
@@ -0,0 +1,22 @@
+// The C Programming Language
+// Excercise getbits() 
+// Author: Jacob Christensen
+
+#include <stdio.h>
+
+unsigned getbits(unsigned x, int p, int n) {
+
+    // get n bits of x from position p
+
+    printf("(%u >> (%d + 1 - %d)) & ~(~0 << %d)\n", x, p, n, n);
+    printf("(%u >> %d) & ~%d\n", x, (p + 1 - n), (~0 << n));
+    printf("%u & %d\n", (x >> (p + 1 - n)), (~(~0 << n)));
+    return (x >> (p + 1 - n)) & ~(~0 << n);
+
+}
+int main(void) {
+
+
+    printf("%u\n", getbits(254u, 6, 2));
+    return 0;
+}
