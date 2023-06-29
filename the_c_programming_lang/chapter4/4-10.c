90041db3e1dbede9b8e9e4926ea8b83fefd8520e
Jacob Christensen <jchristensen888@gmail.com>
2023-06-28 21:47:26 -0600

    WIP chapter4: 4-10

--- the_c_programming_lang/chapter4/4-10.c
+++ the_c_programming_lang/chapter4/4-10.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
 
 #define MAXVAL 100
 #define MAXOP 100
@@ -14,13 +15,40 @@
 #define SETVAR '8'
 #define GETVAR '9'
 
-int buf[BUFSIZE];
+char buf[BUFSIZE];
 int bufp = 0;
 
 int sp = 0;
 double val[MAXVAL];
 double var[26];
 double last;
+
+int gets(char s[]) {
+
+    int character;
+    int i;
+
+    for (i = 0; (character = getchar()) != EOF && character != '\n'; i++) {
+        s[i] = character;
+    }
+    if (character != '\n' && character != EOF) {
+        // We hit the character limit
+        s[i] = '\0';
+        while((character = getchar()) != EOF && character != '\n') {
+            i++;
+        }
+    }
+    if (character == '\n') {
+        s[i] = character;
+        i++;
+        s[i] = '\0';
+    }
+    else {
+        s[i] = '\0';
+    }
+    
+    return i;
+}
 
 void clear(void) {
     sp = 0;
@@ -31,8 +59,8 @@ int getop(char s[]) {
     int i;
     int c;
     int c1;
-
-    while ((s[0] = c = getch()) == ' ' || c == '\t') {
+    
+    while ((s[0] = c = buf[bufp++]) == ' ' || c == '\t') {
         ;
     }
 
@@ -50,32 +78,30 @@ int getop(char s[]) {
     }
 
     i = 0;
-    if ((c == '-') && ((c1 = getch()) == ' ' || c1 == '\n')) {
-        ungetch(c1);
+    if ((c == '-') && ((c1 = buf[bufp + 1]) == ' ' || c1 == '\n')) {
         return '-';
     }
     else if (c == '-' && isdigit(c1)) {
         s[i] = '-';
-        ungetch(c1);
-        c = c1;
+        c = buf[++bufp];
     }
 
     if (isdigit(c)) {
-        while (isdigit(s[++i] = c = getch())) {
+        while (isdigit(s[++i] = c = buf[bufp++])) {
             ;
         }
     }
 
     if (c == '.') {
-        while (isdigit(s[++i] = c = getch())) {
+        while (isdigit(s[++i] = c = buf[bufp++])) {
             ;
         }
     }
 
     s[i] = '\0';
-    if (c != EOF) {
-        ungetch(c);
-    }
+    /* if (c != EOF) { */
+    /*     ungetch(c); */
+    /* } */
 
     return NUMBER;
 
@@ -141,79 +167,84 @@ int main(void) {
     double op1;
     char s[MAXOP];
 
-    while ((type = getop(s)) != EOF) {
-        /* printf("%d\n", sp); */
-        switch (type) {
-            case SETVAR:
-                // Set the variable to top value of the stack
-                var[s[0] - 'A'] = top();
-                break;
-            case GETVAR:
-                // Replace a variable with it's value
-                push(var[s[0] - 'a']);
-                break;
-            case NUMBER:
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                swap();
-                push(pop() - pop());
-                break;
-            case '/':
-                if ((op2 = pop()) != 0.0) {
-                    push(pop() / op2);
-                }
-                else {
-                    printf("error: zero divisor");
-                }
-                break;
-            case '%':
-                if ((op2 = pop()) != 0.0) {
-                    push((double)((int) pop() % (int) op2));
-                }
-                else {
-                    printf("error: zero divisor");
-                }
-                break;
-            case '^':
-                // pow
-                push(pow(pop(), pop()));
-                break;
-            case '\"': 
-                // exp
-                push(exp(pop()));
-                break;
-            case '~':
-                // sin
-                push(sin(pop()));
-                break;
-            case '?':
-                // Print top value
-                printf("%lf\n", top());
-                break;
-            case ':':
-                // Duplicate top value
-                push((top()));
-                break;
-            case '$':
-                // Push last printed value to the stack
-                push(last);
-                break;
-            case '_':
-                // Clear stack
-                clear();
-                break;
-            case '\n':
-                last = op2 = pop();
-                printf("\t%.8g\n", op2);
-                break;
+    while (gets(buf)) {
+
+        printf("%lu\n", strlen(buf));
+        while ((type = getop(s)) != EOF) {
+            /* printf("%d\n", sp); */
+            switch (type) {
+                case SETVAR:
+                    // Set the variable to top value of the stack
+                    var[s[0] - 'A'] = top();
+                    break;
+                case GETVAR:
+                    // Replace a variable with it's value
+                    push(var[s[0] - 'a']);
+                    break;
+                case NUMBER:
+                    push(atof(s));
+                    break;
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '-':
+                    swap();
+                    push(pop() - pop());
+                    break;
+                case '/':
+                    if ((op2 = pop()) != 0.0) {
+                        push(pop() / op2);
+                    }
+                    else {
+                        printf("error: zero divisor");
+                    }
+                    break;
+                case '%':
+                    if ((op2 = pop()) != 0.0) {
+                        push((double)((int) pop() % (int) op2));
+                    }
+                    else {
+                        printf("error: zero divisor");
+                    }
+                    break;
+                case '^':
+                    // pow
+                    push(pow(pop(), pop()));
+                    break;
+                case '\"': 
+                    // exp
+                    push(exp(pop()));
+                    break;
+                case '~':
+                    // sin
+                    push(sin(pop()));
+                    break;
+                case '?':
+                    // Print top value
+                    printf("%lf\n", top());
+                    break;
+                case ':':
+                    // Duplicate top value
+                    push((top()));
+                    break;
+                case '$':
+                    // Push last printed value to the stack
+                    push(last);
+                    break;
+                case '_':
+                    // Clear stack
+                    clear();
+                    break;
+                case '\n':
+                    last = op2 = pop();
+                    printf("\t%.8g\n", op2);
+                    break;
+            }
         }
+        bufp = 0;
     }
 
     return 0;
